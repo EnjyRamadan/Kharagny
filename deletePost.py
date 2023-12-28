a64346785bce4f7582d60ad2db459a3dcfd16bb6
Mazen Ashraf <mezom1644@gmail.com>
2023-12-28 03:03:48 +0200

    1

--- deletePost.py
+++ deletePost.py
@@ -4,4 +4,3 @@ from post import Post
 def removeImageFromPost(postID, imagePath):
     post = Post()
     post.removeFromImages(postID, imagePath)
-
--- readPost.py
+++ readPost.py
@@ -1,9 +1,17 @@
 from post import Post
-
+from io import BytesIO
+from PIL import Image
 
 def getCategoryPost(categoryName):
     temp = Post()
     posts = temp.getPostsByCategory(categoryName)
+    for post in posts:
+        images = []
+        for image in post.getImages():
+            images.append(BytesIO(image))
+            Image.open(image)
+        post.setImages(images)
+
     return posts
 
 
